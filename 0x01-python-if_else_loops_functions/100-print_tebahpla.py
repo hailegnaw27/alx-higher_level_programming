49161178513192d410ecab7c36f5f2621e23a98f
hailegnaw27 <ixvos406@gmail.com>
2023-10-03 14:23:41 -0700

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 for ascii_value in range(122, 96, -1):
-    if ascii_value % 2 == 1:
+    if ascii_value % 2 == 0:
         letter = chr(ascii_value)
     else:
         letter = chr(ascii_value - 32)

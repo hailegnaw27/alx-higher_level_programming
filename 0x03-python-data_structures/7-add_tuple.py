df9430156dac69056325b588942192c170025b4b
hailegnaw27 <ixvos406@gmail.com>
2023-10-08 04:45:42 -0700

    7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,11 +3,11 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     # If tuple_a is smaller than 2, add 0 for missing integers
     if len(tuple_a) < 2:
-        tuple_a += (0,)
+        tuple_a += (0, 0)
 
     # If tuple_b is smaller than 2, add 0 for missing integers
     if len(tuple_b) < 2:
-        tuple_b += (0,)
+        tuple_b += (0, 0)
 
     # Calculate the sum of the first elements and the sum of the second elements
     sum_first = tuple_a[0] + tuple_b[0]

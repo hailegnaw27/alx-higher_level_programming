408e50ff9de138ad47547e9be270fe09649b87d5
hailegnaw27 <ixvos406@gmail.com>
2023-10-08 10:34:52 -0700

    7- ;

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -11,6 +11,5 @@ if __name__ == "__main__":
     tuple_b = (88, 11)
     new_tuple = add_tuple(tuple_a, tuple_b)
     print(new_tuple)
-
     print(add_tuple(tuple_a, (1, )))
     print(add_tuple(tuple_a, ()))

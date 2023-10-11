440a42023b0aeda55fc4d09264471973df209eaf
hailegnaw27 <ixvos406@gmail.com>
2023-10-11 13:35:38 -0700

    8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -5,6 +5,7 @@ def simple_delete(a_dictionary, key=""):
         del a_dictionary[key]
     return a_dictionary
 
+
 if __name__ == "__main__":
     def print_sorted_dictionary(a_dictionary):
         for key in sorted(a_dictionary.keys()):

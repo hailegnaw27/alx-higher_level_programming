ad4b5fc4603e62bd239b1ba640ac6679799e84b0
hailegnaw27 <ixvos406@gmail.com>
2023-10-11 13:24:59 -0700

    3-common_elements.py

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    common_set = set()
+    for element in set_1:
+        if element in set_2:
+            common_set.add(element)
+    return common_set
+
+
+if __name__ == "__main__":
+    set_1 = {"Python", "C", "Javascript"}
+    set_2 = {"Bash", "C", "Ruby", "Perl"}
+    c_set = common_elements(set_1, set_2)
+    print(sorted(list(c_set)))

68d4766b00e3307058404f879ed003fb13560650
hailegnaw27 <ixvos406@gmail.com>
2023-10-25 12:15:45 -0700

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,11 +2,7 @@
 
 class Square:
     """
-    This class represents a square.
+    This class defines a square.
     """
 
-if __name__ == "__main__":
-    my_square = Square()
-    print(type(my_square))
-    print(my_square.__dict__)
-
+    pass

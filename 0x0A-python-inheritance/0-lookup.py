2f15eac9cc483af608f6190a06be2fef64b344fd
hailegnaw27 <ixvos406@gmail.com>
2023-11-06 16:56:49 +0000

    0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,16 @@
+ass MyList(list):
+    """
+    Inherits from the list class and provides additional functionality.
+    """
+
+    def print_sorted(self):
+        """
+        Prints the list in ascending order.
+        """
+        sorted_list = sorted(self)
+        print(sorted_list)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("./tests/1-my_list.txt")

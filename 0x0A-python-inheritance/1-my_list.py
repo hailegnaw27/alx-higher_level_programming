96f71c9bb290d119d9f992f2d79688c8cf8c87a4
hailegnaw27 <ixvos406@gmail.com>
2023-11-06 19:37:23 +0000

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 
-"""
-MyList module
-"""
 
 class MyList(list):
     """
-    MyList class that inherits from list
+    MyList class that inherits from list.
     """
 
     def print_sorted(self):
         """
-        Public instance method that prints the list in ascending order
+        Public instance method that prints the list in ascending order.
         """
         sorted_list = sorted(self)
         print(sorted_list)

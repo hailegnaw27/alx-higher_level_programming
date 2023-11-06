547ab9d38629c271a0cd6f812861755dc691bfda
hailegnaw27 <ixvos406@gmail.com>
2023-11-06 17:41:52 +0000

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python3
 """
-This module contains a class MyList that is a sub-class of the list class.
+This is a module containing the MyList class
+which inherits from the list class and has a
+method that prints a sorted list.
 """
 
 
 class MyList(list):
     """
-    A sub-class of the built-in list class
+    A class inheriting from the list class
+
+    Methods:
+        print_sorted(self)
     """
 
     def print_sorted(self):
         """
-        Prints a sorted instance of `MyList`
+        Prints the list in ascending order
         """
         print(sorted(self))
+
+
+if __name__ == "__main__":
+    my_list = MyList([1, 2, 3, 5, 4, 6, 7, 9])
+    print(my_list)
+    my_list.print_sorted()

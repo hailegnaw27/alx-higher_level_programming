d6f21635c95b92eac516f276403af670837cddcf
hailegnaw27 <ixvos406@gmail.com>
2023-12-12 13:28:46 -0800

    100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -10,4 +10,3 @@ ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode
 
 -- Convert the field to UTF8
 ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-

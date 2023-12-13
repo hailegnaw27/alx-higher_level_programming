7684c6fe61ae99d0e0481552691dea3fa1fe5182
hailegnaw27 <ixvos406@gmail.com>
2023-12-13 03:04:04 -0800

    103-max_state.sql

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,8 +1,6 @@
 -- This script displays the max temperature of each state ordered by State name
-SELECT `city`, AVG(`value`) AS `avg_temp`
+SELECT `state`, MAX(`value`) AS `max_temp`
 FROM `temperatures`
-WHERE `month` = 7 OR `month` = 8
-GROUP BY `city`
-ORDER BY `avg_temp` DESC
-LIMIT 3;
+GROUP BY `state`
+ORDER BY `state`;
 

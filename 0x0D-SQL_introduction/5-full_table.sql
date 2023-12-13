3fcf5e2e11ff46792a630af6cf89fa904dca35f2
hailegnaw27 <ixvos406@gmail.com>
2023-12-13 03:07:39 -0800

    5-full_table.sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,9 +1,2 @@
--- 5-full_table.sql
-
--- This script prints the full description of the table first_table from the specified database
-
-SELECT TABLE_NAME, CREATE_TABLE
-FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_SCHEMA = 'hbtn_0c_0'
-AND TABLE_NAME = 'first_table';
-
+---  This script prints the full description of the table first_table from the specified database
+SHOW CREATE TABLE `first_table`;

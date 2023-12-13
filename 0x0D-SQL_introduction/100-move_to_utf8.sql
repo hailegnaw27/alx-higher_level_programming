430d45e30a13511f657ade61056e5bb533c389d9
hailegnaw27 <ixvos406@gmail.com>
2023-12-13 02:49:37 -0800

    100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -3,10 +3,10 @@
 -- This script converts the hbtn_0c_0 database to UTF8
 
 -- Convert the database to UTF8
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE 'hbtn_0c_0'
 
 -- Convert the table to UTF8
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE 'first_table'
 
 -- Convert the field to UTF8
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,8 +1,5 @@
 -- 3-list_tables.sql
 
 -- This script lists all the tables in the specified database
-
 USE dbname;
-
 SHOW TABLES;
-

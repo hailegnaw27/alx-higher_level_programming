a5431c17fe968f8bf4dcb330dc96d4080c414e70
hailegnaw27 <ixvos406@gmail.com>
2023-12-14 04:42:26 -0800

    0-privileges.sql

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,7 @@
+-- Lists privileges of user_0d_1 and user_0d_2
+
+-- Privileges for user_0d_1
+SELECT * FROM mysql.user WHERE User = 'user_0d_1';
+
+-- Privileges for user_0d_2
+SELECT * FROM mysql.user WHERE User = 'user_0d_2';

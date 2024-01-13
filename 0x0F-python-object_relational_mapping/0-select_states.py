ea4e42116d6966bbe5b1b071328c1ab9cab6820e
hailegnaw27 <ixvos406@gmail.com>
2024-01-13 04:59:25 -0800

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,6 +2,7 @@
 """
 list all stattrs
 """
+
 import MySQLdb
 import sys
 
@@ -9,12 +10,12 @@ if __name__ == '__main__':
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
+
     db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database)
     cursor = db.cursor()
+    
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
     rows = cursor.fetchall()
     
-
     for row in rows:
         print(row)

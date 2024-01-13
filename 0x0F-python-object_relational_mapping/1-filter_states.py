137fdd71c6aca53fa028460977e6a9eee449fc0e
hailegnaw27 <ixvos406@gmail.com>
2024-01-13 05:12:59 -0800

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,25 +3,14 @@ import MySQLdb
 import sys
 
 if __name__ == '__main__':
-    # Connect to the MySQL server
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
     db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database)
-    
-    # Create a cursor object to interact with the database
     cursor = db.cursor()
-    
-    # Execute the SQL query to retrieve the states starting with 'N'
     cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-    
-    # Fetch all the rows returned by the query
     rows = cursor.fetchall()
-    
-    # Print the results
     for row in rows:
         print(row)
-    
-    # Close the cursor and database connection
     cursor.close()
     db.close()

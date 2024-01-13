fee174b4338e131fa1304f4c01246f980521325c
hailegnaw27 <ixvos406@gmail.com>
2024-01-13 06:13:46 -0800

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,33 +1,32 @@
 #!/usr/bin/python3
 
 """
-This script filters states by user input using MySQLdb.
+Filter states by user input
 """
 
 import sys
 import MySQLdb
 
-# Retrieve command-line arguments
-mysql_username = sys.argv[1]
-mysql_password = sys.argv[2]
-database_name = sys.argv[3]
-state_name = sys.argv[4]
+if __name__ == "__main__":
+    # Retrieve command-line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
 
-# Establish a connection to the MySQL server
-db = MySQLdb.connect(host='localhost', port=3306, user=mysql_username, passwd=mysql_password, db=database_name)
+    # Establish a connection to the MySQL server
+    db = MySQLdb.connect(user=mysql_username, passwd=mysql_password, db=database_name)
+    cursor = db.cursor()
 
-# Create a cursor object to execute SQL queries
-cursor = db.cursor()
+    # Execute the SQL query
+    query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id".format(state_name)
+    cursor.execute(query)
 
-# Execute the SQL query
-query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-cursor.execute(query, (state_name,))
+    # Fetch the results and display them
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
 
-# Fetch the results and display them
-results = cursor.fetchall()
-for row in results:
-    print(row)
-
-# Close the cursor and database connection
-cursor.close()
-db.close()
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()

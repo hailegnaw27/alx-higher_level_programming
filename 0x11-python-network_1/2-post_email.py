b0d34ea1f32831389545027ab5bd4ec86d25c4f8
hailegnaw27 <ixvos406@gmail.com>
2024-01-26 18:11:54 +0000

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,6 +6,7 @@ import sys
 url = sys.argv[1]
 email = sys.argv[2]
 
+if __name__ == "__main__":
 data = urllib.parse.urlencode({'email': email}).encode('utf-8')
 req = urllib.request.Request(url, data=data, method='POST')
 

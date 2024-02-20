e53dbde35a23490b31c22b59288e2961337bdd6a
hailegnaw27 <ixvos406@gmail.com>
2024-02-20 11:15:45 -0800

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,22 +1,8 @@
-/*
-  File: 0-readme.js
-  Description: A simple JavaScript file for reading and displaying the content of a Readme file.
-  Author: Your Name
-  Date: Current Date
-*/
+#!/usr/bin/node
 
 const fs = require('fs');
 
-// Function to read and display the content of a Readme file
-function readReadmeFile() {
-  fs.readFile('README.md', 'utf8', (err, data) => {
-    if (err) {
-      console.error('Error reading the Readme file:', err);
-    } else {
-      console.log('Readme file content:\n', data);
-    }
-  });
-}
-
-// Call the function to read the Readme file
-readReadmeFile();
+// Read and display the content of the specified file
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
+});
